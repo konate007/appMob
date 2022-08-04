596f806b46d213508ee7194db909cb92252c1c73
KONATE Maliki <maliki@diotali.com>
2022-08-04 00:24:07 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { SearchBar } from "react-native-elements";
+import { useState } from 'react';
+
 
 export default function App() {
+  const [searchValue, setSearchValue] = useState("")
+  const searchFunction = (text: string) => {
+    setSearchValue(text)
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={{ padding: 10, backgroundColor: 'orange' }}>
+        <Text style={{ color: '#fff' }}>Appeler pour comander</Text>
+      </TouchableOpacity>
+      <View style={{ flexDirection: 'row' }}>
+        <Image source={require('./assets/logo.png')} style={{ width: 100, height: 50 }} />
+        <TouchableOpacity onPress={() => alert("Shopping Bag")}>
+          <MaterialIcons name='shop' size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert("Bag...")}>
+          <MaterialIcons name='shop' size={30} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert("User account")}>
+          <MaterialIcons name='person' size={30} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <TouchableOpacity onPress={() => alert("Menu ...")}>
+          <MaterialIcons name='menu' size={30} />
+        </TouchableOpacity>
+        <View>
+          <TextInput
+            placeholder='Rechercher'
+            value={searchValue}
+            onChangeText={(searchValue) => searchFunction(searchValue)}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
